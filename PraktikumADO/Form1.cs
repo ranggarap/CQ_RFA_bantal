4ea2e23e7349ec8e03ffcfa729be7f0f018f00a8
ranggarap <fadhilahrangga26@gmail.com>
2026-03-31 14:06:06 +0700

    menambahkan variabel koneksi

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -6,6 +6,8 @@ namespace PraktikumADO
 {
     public partial class Form1 : Form
     {
+        SqlConnection conn;
+        SqlCommand cmd;
         public Form1()
         {
             InitializeComponent();
